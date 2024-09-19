198af53b51f7d3958d35379eda07d77f15d770e0
Evanthia Palatou <p7dt68xx@students.codeinstitute.net>
2024-09-19 08:05:32 +0000

    Fix bug with button position in index.html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
-// Sound effects
+// Sound effects for each movement, the win condition and the draw
 const moveSound = new Audio('assets/sounds/move.mp3');
 const winSound = new Audio('assets/sounds/win.mp3');
 const drawSound = new Audio('assets/sounds/draw.mp3');
 
-let board = ['', '', '', '', '', '', '', '', ''];
-let currentPlayer = 'X';
-let gameActive = true;
-let scoreX = 0;
-let scoreO = 0;
+// we use the following let variables for:
+let board = ['', '', '', '', '', '', '', '', '']; // empty board 
+let currentPlayer = 'X'; // active player, always X starts first
+let gameActive = true; // checks the game's status
+let scoreX = 0; // X's score
+let scoreO = 0; // O's score
+let isMuted = false; // Variable to track the current mute state
 
-const statusDisplay = document.getElementById('status');
-const scoreXDisplay = document.getElementById('scoreX');
-const scoreODisplay = document.getElementById('scoreO');
+const statusDisplay = document.getElementById('status'); // dynamic game board
+const scoreXDisplay = document.getElementById('scoreX'); // stores X's total score
+const scoreODisplay = document.getElementById('scoreO'); // stores O's total score
 const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -23,7 +25,9 @@ const winningConditions = [
     [2, 4, 6]
 ];
 
-// Handle a cell click
+/** A function that handles a cell click and takes as a parameter an event,
+* checks whether a cell is empty or not in order to decide the game activity status. 
+*/
 function handleCellClick(event) {
     const clickedCellIndex = event.target.getAttribute('data-index');
 
@@ -38,7 +42,9 @@ function handleCellClick(event) {
     checkForWinner();
 }
 
-// Update the board
+/** Update the board function uses the ternary operator to decide player's turn is to mark the board.
+* 
+*/
 function updateBoard(index) {
     board[index] = currentPlayer;
     document.querySelector(`[data-index="${index}"]`).innerText = currentPlayer;
@@ -119,6 +125,8 @@ function newGame() {
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
 document.getElementById('reset').addEventListener('click', resetGame);
 document.getElementById('newGame').addEventListener('click', newGame);
+// Add event listener to the mute button
+document.getElementById('muteButton').addEventListener('click', toggleMute);
 
 // Initialize status
 statusDisplay.innerText = `It's ${currentPlayer}'s turn`;
