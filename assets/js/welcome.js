252e3a9c1a19bbcfb321d2d37522e7245a136038
Evanthia Palatou <p7dt68xx@students.codeinstitute.net>
2024-09-21 11:43:46 +0000

    Fix error in Nu Html Checker

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -3,15 +3,18 @@ async function init() {
 
   await sleep(1000);
   node.innerText = "";
-  await node.type("Welcome, ");
+  await node.type("");
 
   while (true) {
-    await node.type("Player!");
+    await node.type("Welcome,to_Tic-Tac-Toe_Game");
     await sleep(2000);
-    await node.delete("Player!");
-    await node.type("toTicTacToe");
+    await node.delete("Welcome,to_Tic-Tac-Toe_Game");
+    await node.type("it_is_recommended_you_play");
     await sleep(2000);
-    await node.delete("toTicTacToe");
+    await node.delete("it_is_recommended_you_play");
+    await node.type("in_laptop_or_computer!");
+    await sleep(2000);
+    await node.delete("in_laptop_or_computer!");
   }
 }
 
